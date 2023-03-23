44a287b23432d5e4029ec060c15f6721766e2bab
Yogesh Parihar <yogeshparihar818@gmail.com>
2023-03-23 11:30:09 +0530

    app.py is ready

--- app.py
+++ app.py
@@ -14,26 +14,30 @@ scaler = pickle.load(open('scaler.pkl','rb'))
 def home():
     return render_template('home.html')
 
-# @app.route('/predict_api',methods=['POST'])
+# @app.route('/predict_api_1',methods=['POST'])
 # def predict_api():
 #     data = request.json['data']
 #     print(data)
-#     print(np.array(list(data.values())).reshape(1,-1))
-#     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-#     output = regmodel.predict(new_data)
+#     print(np.array([float(x) for x in data.values()]).reshape(1,-1))
+#     new_data = scaler.transform(np.array([float(x) for x in data.values()]).reshape(1,-1))
+#     output = xgbmodel.predict(new_data)
 #     print(output[0])
 
 #     return jsonify(output[0])
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     data = [float(x) for x in request.form.values()]
-#     final_input = scaler.transform(np.array(data).reshape(1,-1))
-#     print(final_input)
-#     output = regmodel.predict(final_input)[0]
-#     return render_template("home.html",prediction_text="The House Price Predicted is {}".format(output))
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = xgbmodel.predict(final_input)[0]
+    if output==0:
+        output_text = "No Rain"
+    else:
+        output_text = "Rain"
+    return render_template("home.html",prediction_text=output_text)
 # @app.route('/trial')
 # def trial():
 #     return render_template('trial.html')
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
